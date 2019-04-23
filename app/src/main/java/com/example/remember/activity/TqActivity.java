91d6daac00e6260b1ca8f9601e60968869e4efb9
muwei <1498271853@qq.com>
2019-04-23 16:09:27 +0800

    从服务器上获取天气相关的的地理位置信息 提供碎片编织选择

--- app/src/main/java/com/example/remember/activity/TqActivity.java
+++ app/src/main/java/com/example/remember/activity/TqActivity.java
@@ -21,7 +21,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class TqActivity extends AppCompatActivity implements View.OnClickListener {
+public class TqActivity extends AppCompatActivity{
 
     TextView responseText;
 
@@ -29,81 +29,7 @@ public class TqActivity extends AppCompatActivity implements View.OnClickListene
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tq);
-        responseText = (TextView)findViewById(R.id.response_text);
-        Button sendRequest = (Button)findViewById(R.id.send_request);
-        sendRequest.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.send_request){
-            sendRequestWithOkHttp();
-        }
-    }
-
-    private void sendRequestWithOkHttp(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try{
-                    OkHttpClient client = new OkHttpClient();
-                    Request request = new Request.Builder().url("http://www.baidu.com").build();
-                    Response response = client.newCall(request).execute();
-                    String responseData = response.body().string();
-                    showResponse(responseData);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
-
-    private void sendRequestWithHttpURLConnection(){
-        //开启线程发起网络请求
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                HttpURLConnection connection = null;
-                BufferedReader reader = null;
-                try {
-                    URL url = new URL("https://www.baidu.com");
-                    connection = (HttpURLConnection)url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.setConnectTimeout(8000);
-                    connection.setReadTimeout(8000);
-                    InputStream in = connection.getInputStream();
-                    reader = new BufferedReader(new InputStreamReader(in));
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while((line = reader.readLine())!=null){
-                        response.append(line);
-                    }
-                    showResponse(response.toString());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                   if (reader != null){
-                       try {
-                           reader.close();
-                       }catch (Exception e){
-                           e.printStackTrace();
-                       }
-                   }
-                   if (connection != null){
-                       connection.disconnect();
-                    }
-                }
-            }
-        }).start();
-    }
-
-    private void showResponse(final  String response){
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                responseText.setText(response);
-            }
-        });
-    }
 
 }
