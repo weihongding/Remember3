280eb224eae32f17e2695a3abf5ea7063713c488
muwei <1498271853@qq.com>
2019-05-01 17:53:34 +0800

    原型基本完成

--- app/src/main/java/com/example/remember/adapter/BwlAdapter.java
+++ app/src/main/java/com/example/remember/adapter/BwlAdapter.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -74,5 +75,7 @@ public class BwlAdapter extends RecyclerView.Adapter<BwlAdapter.ViewHolder>{
             tv_time = (TextView)view.findViewById(R.id.text_bwl_item_time);
         }
     }
+
+
 
 }
